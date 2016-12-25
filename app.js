bd968b07aeca64ca355dc842523a5b9a14d19cbf
gujunmin <3013366498@qq.com>
2016-12-25 22:06:58 +0800

    update 20161225

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var path = require('path');
 var logger = require('morgan');
+var bodyParser = require('body-parser');
 
 var port = process.env.PORT || 3000;
 var app = express();
@@ -9,6 +10,8 @@ app.set('views', path.join(__dirname, 'views/pages'));
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extend: false }));
 app.use(express.static(path.join(__dirname, 'bower_components')));
 
 // index page
@@ -41,28 +44,55 @@ app.get('/', function(req, res) {
 			poster:'http://r3.ykimg.com/05410408548589706A0A4160AF2742DF'
 		}]
     });
-})
+});
 
 // detail page
 app.get('/movie/:id', function(req, res) {
     res.render('detail', {
-        title: 'imooc 详情页'
+        title: 'imooc 详情页',
+        movie: {
+            title: '奇妙世纪 08 梦的还原器',
+            doctor: '程亮/林博',
+            country: '大陆',
+            language: '汉语',
+            year: 2014,
+            poster: 'http://r3.ykimg.com/05410408548589706A0A4160AF2742DF',
+            flash: 'http://player.youku.com/player.php/sid/XODQ0NDk4MTA0/v.swf',
+            summary: '《奇妙世纪》是由啼声影视与优酷出品共同打造的中国首部原创都市奇幻单元剧。'
+        }
     });
-})
+});
 
 // admin page
 app.get('/admin/movie', function(req, res) {
     res.render('admin', {
-        title: 'imooc 后台录入页'
+        title: 'imooc 后台录入页',
+        movie: {
+            title: '',
+            doctor: '',
+            country: '',
+            language: '',
+            year: '',
+            summary: '',
+            poster: '',
+            flash: ''
+        }
     });
-})
+});
 
 // list
 app.get('/admin/list', function(req, res) {
     res.render('list', {
-        title: 'imooc 列表页'
+        title: 'imooc 列表页',
+        movies: [{
+            _id: 1,
+            title: '奇妙世纪 08 梦的还原器',
+            doctor: '程亮/林博',
+            country: '大陆',
+            year: 2014,
+        }]
     });
-})
+});
 
 app.listen(port);
 
