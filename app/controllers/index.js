6fe58a4365694071718dca44f25aa9acd0f382c2
gujunmin <3013366498@qq.com>
2017-02-02 22:36:34 +0800

    update 170202 22:36

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -21,13 +21,56 @@ exports.index = function(req, res) {
 // search page
 exports.search = function(req, res) {
     var catId = req.query.cat;
-    var page = req.query.p;
-    var index = page * 2;
+    var q = req.query.q;
+    console.log('req.query.p: ', req.query.p);
+    var page = parseInt(req.query.p, 10) || 0;
+    // TODO: 查看page的值，在搜索的时候是多少？
+    var count = 2;
+    var index = page * count;
 
-    Category
-        .find({id: catId})
-        .populate({
-            path: 'movies',
-            select: 'title poster',
-        })
+    if (catId) {
+
+        Category
+            .find({_id: catId})
+            .populate({
+                path: 'movies',
+                select: 'title poster'
+            }).
+            exec(function(err, categories) {
+                if (err) {
+                    console.log(err);
+                }
+                var category = categories[0] || {};
+                var movies = category.movies || [];
+                var results = movies.slice(index, index + count);
+
+                res.render('results', {
+                    title: 'imooc 结果列表页面',
+                    keyword: category.name,
+                    currentPage: (page + 1),
+                    query: 'cat=' + catId,
+                    totalPage: Math.ceil(movies.length / count),
+                    movies: results
+                });
+            });
+    } else {
+        Movie
+            .find({name: q})
+            .exec(function(err, movies) {
+                if (err) {
+                    console.log(err);
+                }
+
+                var results = movies.slice(index, index + count);
+
+                res.render('results', {
+                    title: 'imooc 结果页面',
+                    keyword: q,
+                    currentPage: (page + 1),
+                    query: 'q=' + q,
+                    totalPage: Math.ceil(movies.length / count),
+                    movies: results
+                });
+            });
+    }
 };
