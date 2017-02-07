b13f1cf8ad63973df43251a7fac5b68bb250745b
gujunmin <3013366498@qq.com>
2017-02-07 12:10:42 +0800

    update 170207 12:10

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -5,7 +5,7 @@ var Category = require('../models/category');
 exports.index = function(req, res) {
     Category
         .find({})
-        .populate({path: 'movies', options: {limit: 5}})
+        .populate({path: 'movies', options: {limit: 6}})
         .exec(function(err, categories) {
             if (err) {
                 console.log(err);
